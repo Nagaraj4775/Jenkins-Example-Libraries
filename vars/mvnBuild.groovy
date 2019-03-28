cc9dd0d4b9d239c11de981fef0fa0dcae694355d
Nagaraj <rapellinagaraj@gmail.com>
2019-03-28 08:05:05 +0530

    Update mvnBuild.groovy

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,6 +1,6 @@
 def call(Map pipelineParams){
 
-  echo "pipelineParams.BuildConfiguration"
+  echo "$pipelineParams.BuildConfiguration"
 echo "We are in Jenkins build stage"
 sh "mvn clean package -Dmaven.test.failure.ignore=true"
 }
