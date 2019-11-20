7aea7989885f9f900216645eb9bc2536ff303266
Nagaraj <rapellinagaraj@gmail.com>
2019-11-20 15:33:02 +0530

    Create getWorkSpace.groovy

---
+++ vars/getWorkSpace.groovy
@@ -0,0 +1,8 @@
+def getWorkSpace(environment,appName){
+    if(environment == 'uat'){
+        return "/u01/app/prod/build/${environment}/${appName}"
+    }
+    else{
+        return "/u01/app/nonprod/build/${environment}/${appName}"
+    }
+}
