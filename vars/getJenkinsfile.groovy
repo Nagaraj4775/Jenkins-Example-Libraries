5bf0a9227e46dada321f4bb286629f72256d113e
Nagaraj <rapellinagaraj@gmail.com>
2019-12-12 12:31:21 +0530

    Update getJenkinsfile.groovy

--- vars/getJenkinsfile.groovy
+++ vars/getJenkinsfile.groovy
@@ -16,8 +16,7 @@ agent any
 				stageName = "${STAGE_NAME}"
 				echo "We are in git clone"	
 					checkout scm	
-					changeSet = build.changeSet
-					echo "${changeSet}"
+					
 			  }
 		    }
 		}
@@ -31,6 +30,27 @@ agent any
 			echo "$pipelineParams.buildCommand"
 			echo "${BRANCH_NAME}"
 			echo "${currentBuild.changeSets}"
+			
+			
+			<!-- CHANGE SET -->
+<% changeSet = build.changeSet
+if (changeSet != null) {
+hadChanges = false %>
+<h2>Changes</h2>
+<ul>
+<% changeSet.each { cs ->
+hadChanges = true
+aUser = cs.author %>
+<li>Commit <b>${cs.revision}</b> by <b><%= aUser != null ? aUser.displayName :      it.author.displayName %>:</b> (${cs.msg})
+<ul>
+<% cs.affectedFiles.each { %>
+<li class="change-${it.editType.name}"><b>${it.editType.name}</b>: ${it.path}                              </li> <%  } %> </ul>   </li> <%  }
+
+ if (!hadChanges) { %>  
+  <li>No Changes !!</li>
+ <%  } %>   </ul> <% } %>
+	
+	
 		}
 		}
 	}
