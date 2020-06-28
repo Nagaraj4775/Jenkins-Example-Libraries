88bdd0290177bb996c2e5750cacfdf2eae086b87
Nagaraj <rapellinagaraj@gmail.com>
2020-06-28 18:59:11 +0530

    Update testPipeline.groovy

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -16,9 +16,9 @@ def call(body){
                 }
             }
             stage('sonar Analysis'){
-                when {
-					environment name: 'config.publishToArtifactory', value: 'true'
-				}
+                 when {
+			environment name: 'config.codeAnalysis', value: 'true'
+		}
                 steps{
                     script{
                         echo "Running Sonar Analysis..."             
